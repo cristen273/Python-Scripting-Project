14febaa273bf947482ded364254fb2f48e8cf57c
Cristen Gladius <94090825+cristen273@users.noreply.github.com>
2025-07-10 22:13:47 +0530

    Update get_game_data.py

--- get_game_data.py
+++ get_game_data.py
@@ -23,6 +23,11 @@ def find_all_game_paths(source):
 
     return game_paths
 
+#In this os.walk(source) generates a tuple for ach directory it visits. The tupe has the following (root, dirs., files)
+#Where:
+#•	root: The current directory path being walked.
+#•	dirs: A list of subdirectories in root.
+#•	files: A list of files in root.
 
 def get_name_from_paths(paths, to_strip):
     new_names = []
@@ -32,7 +37,12 @@ def get_name_from_paths(paths, to_strip):
         new_names.append(new_dir_name)
 
     return new_names
+#os.path.split(path) splits the path into two parts:
+#Head: The path leading up to the last component.
+#Tail: The last component (usually a file or directory name).
 
+#The underscore _ is a convention in Python to indicate that the value is being ignored.
+#dir_name receives the tail, which is the name of the directory or file at the end of the path.
 
 def create_dir(path):
     if not os.path.exists(path):
@@ -99,11 +109,16 @@ def main(source, target):
     json_path = os.path.join(target_path, "metadata.json")
     make_json_metadata_file(json_path, new_game_dirs)
 
+#Whatever is inside this main func will only be executed if this file is run directly
+#When we just import the python file to re-use functions and classes, we don’t want the entire contents to run, in such cases this is helpful
 
 if __name__ == "__main__":
     args = sys.argv
     if len(args) != 3:
         raise Exception("You must pass a source and target directory - only.")
-
+    #what this effectively does is when we run a cli command as python filename data new_data
+    #the output when we print args will be filename, data, new_data
+    #Now that I want to get the source and target value separately I parse the line and get the values required
+    
     source, target = args[1:]
     main(source, target)
